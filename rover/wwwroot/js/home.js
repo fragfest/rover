3583a643c0141dae1bcfd0f5000d350b447f7858
cadde <carl.adde@gmail.com>
2020-02-07 11:23:25 -0700

    added vue, xhr to webSvc

--- rover/wwwroot/js/home.js
+++ rover/wwwroot/js/home.js
@@ -1,1 +1,34 @@
-﻿console.log('home.js')
+﻿var home = new Vue({
+    el: '#home',
+    data: {
+        message: 'Hello Vue!'
+    },
+    mounted: function () {
+        console.log('VUE mounted');
+        this.serverGetGrid(10, 10);
+    },
+
+    methods: {
+        serverGetGrid: function serverGetGrid(width, height) {
+            $.ajax({
+                url: 'https://localhost:5003/roverimages',
+                //data: JSON.stringify({
+                //    var1: this.pageUrl,
+                //}),
+                //dataType: 'json',
+                //contentType: 'application/json; charset=utf-8',
+                type: 'GET',
+                headers: {},
+                success: function (data, status, xhr) {
+                    console.log('SUCCESS')
+                    //resolve();
+                },
+                error: function (xhr) {
+                    console.error('FAIL')
+                    //reject(new Error('save status ' + xhr.status + ': ' + xhr.responseText));
+                }
+            });
+        }
+    }
+
+});
--- webSvc/Startup.cs
+++ webSvc/Startup.cs
@@ -25,6 +25,7 @@ namespace webSvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
         }
 
@@ -35,6 +36,10 @@ namespace webSvc
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(
+                options => options.WithOrigins("https://localhost:5000").AllowAnyMethod()
+            );
 
             app.UseHttpsRedirection();
 
